cba5add06f1a31daec8095b47436e42bd8569f00
Katiuska Alicea <katiuskaalicea@gmail.com>
2021-01-24 12:15:09 -0800

    get project by ID works

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -13,4 +13,22 @@ router.get(`/`, async (req, res) => {
   }
 });
 
+router.get(`/:id`, async (req, res) => {
+  const { id } = req.params;
+  const project = await Projects.get(id);
+  if (!project) {
+    res.status(400).json({ message: "Could not find a project with that ID" });
+  } else {
+    try {
+      res.status(200).json(project);
+    } catch (err) {
+      res.status(500).json({ message: "server error" });
+    }
+  }
+});
+
+// router.post(`/`, async (req, res) => {
+
+// })
+
 module.exports = router;
