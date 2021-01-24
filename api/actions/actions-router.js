a92dd9667ab022cd808bb73cceb35e9eeb7b9a5b
Katiuska Alicea <katiuskaalicea@gmail.com>
2021-01-23 23:16:11 -0800

    can get array of actions

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -5,15 +5,28 @@ const router = express.Router();
 
 const Actions = require("./actions-model");
 
-router.get(`/:id`, (req, res) => {
+router.get(`/`, async (req, res) => {
+  //   const { id } = req.params;
+  try {
+    const actions = await Actions.get();
+    res.status(200).json(actions);
+  } catch (err) {
+    res.status(500).json({ message: "error getting the actions" });
+  }
+});
+
+router.get(`/:id`, async (req, res) => {
   const { id } = req.params;
-  const action = Actions.get(id)
-    .then((action) => res.status(200).json(action))
-    .catch((err) =>
-      res
-        .status(500)
-        .json({ message: "there was a problem getting the action" })
-    );
+  const action = await Actions.get(id);
+  if (!action) {
+    res.status(404).json({ message: "there is no action by that ID" });
+  } else {
+    try {
+      res.status(200).json(action);
+    } catch (err) {
+      res.status(500).json({ message: "there was a problem with the server" });
+    }
+  }
 });
 
 module.exports = router;
