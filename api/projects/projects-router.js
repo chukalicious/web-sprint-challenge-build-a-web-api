fbdcba31347c0968d288178bc35f361fb4fba1ad
Katiuska Alicea <katiuskaalicea@gmail.com>
2021-01-24 12:40:00 -0800

    post new project works

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -27,8 +27,21 @@ router.get(`/:id`, async (req, res) => {
   }
 });
 
-// router.post(`/`, async (req, res) => {
-
-// })
+router.post(`/`, async (req, res) => {
+  const { id } = req.params;
+  const projectBody = req.body;
+  if (!projectBody.name || !projectBody.description) {
+    res
+      .status(400)
+      .json({ message: "must include a project name and description" });
+  } else {
+    try {
+      const newProject = await Projects.insert(projectBody, id);
+      res.status(202).json(newProject);
+    } catch (err) {
+      res.status(500).json({ message: "could not add new project" });
+    }
+  }
+});
 
 module.exports = router;
