8e677203bf0b87391fb699a3fb9d9b68b23366d6
Katiuska Alicea <katiuskaalicea@gmail.com>
2021-01-24 12:06:45 -0800

    get to the projects array works

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -4,4 +4,13 @@ const express = require("express");
 const router = express.Router();
 const Projects = require("./projects-model");
 
+router.get(`/`, async (req, res) => {
+  try {
+    const projects = await Projects.get();
+    res.status(200).json(projects);
+  } catch (err) {
+    res.status(500).json({ message: "no projects here" });
+  }
+});
+
 module.exports = router;
